a74bc29003ca100e8410c02bdbf69fc942a12e14
ubamarvin <ubamarvin95@gmail.com>
2024-03-15 14:36:09 +0100

    edit script js: factorized playround() add evaluateRound() add computerScore adjusted playGame()

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const rockPaperScissor = ["rock", "paper", "scissor"];
-        const noOfGames = 5;
+        const noOfRounds = 5;
         let playerScore = 0;
+        let computerScore = 0;
         
         function getUserChoice () {
             userChoice = prompt("Enter either Rock paper or Scissor");
@@ -12,49 +13,47 @@ const rockPaperScissor = ["rock", "paper", "scissor"];
             return rockPaperScissor[randomIndex];
         };
 
-
-        function playRound(playerSelection, computerChoice) {
-
-            //User wins
-            if (playerSelection == "rock" && computerChoice == "scissor") {
-                console.log("Round Won, " + playerSelection + " beats " + computerChoice);
+        //remove {}
+        function evaluateRound(player1choice, player2choice) {
+            if (player1choice == "rock" && player2choice == "scissor") {
                 return 1;
             }
-            else if (playerSelection == "paper" && computerChoice == "rock") {
-                console.log("Round Won, " + playerSelection + " beats " + computerChoice);
+            else if (player1choice == "paper" && player2choice == "rock") {
                 return 1;
             }
-            else if (playerSelection == "scissor" && computerChoice == "paper") {
-                console.log("Round Won, " + playerSelection + " beats " + computerChoice);
+            else if (player1choice == "scissor" && player2choice == "paper") {
                 return 1;
             }
-            //user draws
-            else if (playerSelection == computerChoice) {
-                console.log("Draw!, " + playerSelection + " draws " + computerChoice);
+            else if (player1choice === player2choice) {
                 return 0;
             }
-            //user looses
-            else if (playerSelection == "rock" && computerChoice == "paper") {
-                console.log("Round Lost, " + playerSelection + " looses to " + computerChoice);
-                return -1;
-            }
-            else if (playerSelection == "paper" && computerChoice == "scissor") {
-                console.log("Round Lost, " + playerSelection + " looses to " + computerChoice);
-                return -1;
-            }
-            else if (playerSelection == "scissor" && computerChoice == "rock") {
-                console.log("Round Lost, " + playerSelection + " looses to " + computerChoice);
-                return -1;
-            }
-                
+            else
+                return 0;
+        };
+
+
+        function playRound(playerChoice, computerChoice) {
+            console.log(playerChoice + " vs " + computerChoice);
+            playerScore += evaluateRound(playerChoice, computerChoice);
+            computerScore += evaluateRound(computerChoice,playerChoice);   
         };
 
 
         function playGame() {
-            for (i=0 ; i < noOfGames; i++) {
-                playerScore += playRound(getUserChoice(),getComputerChoice());
+            for (i=0 ; i < noOfRounds; i++) {
+                playRound(getUserChoice(),getComputerChoice());
             }
-            console.log(playerScore > 0 ? "User won" : "Computer won" );
+            console.log(playerScore);
+            console.log(computerScore);
+
+            console.log(playerScore > computerScore ? "User won with " + playerScore + " points."  
+            : "Computer won with "  + computerScore + " points." );
         };
+
+        
         //Main 
         playGame();
+
+        
+
+       
